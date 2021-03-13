b5c2430f02de583579a1edbcaaa60676125fda8c
Zhurawlenokk <zhuravlevamary36@gmail.com>
2021-03-13 10:23:06 +0400

    init: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,84 @@
+exports.min = function min(array) {
+  if (Array.isArray(array) === false) {
+    return 0
 
-exports.min = function min (array) {
-  return 0;
+  } else if (array === undefined) {
+    return 0
+
+  } else if (array.length === 0) {
+    return 0
+
+  } else {
+    let newArr = array.filter(item => {
+      if (item < 0) {
+        return item
+      }
+    })
+
+    let min = newArr.reduce((acc,val)=> (acc < val) ? acc:val);
+
+    return min
+
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+
+
+
+
+
+exports.max = function max(array) {
+  if (Array.isArray(array) === false) {
+    return 0
+
+  } else if (array === undefined) {
+    return 0
+
+  } else if (array.length === 0) {
+    return 0
+
+  } else {
+    let newArr = array.filter(item => {
+      if (item > 0) {
+        return item
+      }
+    })
+
+    console.log(newArr);
+
+    let max = newArr.reduce((acc,val) => (acc > val) ? acc:val);
+
+    return max
+
+  }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = function avg(array) {
+
+  if (Array.isArray(array) === false) {
+    return 0
+
+  } else if (array === undefined) {
+    return 0
+
+  } else if (array.length === 0) {
+    return 0
+
+  } else {
+    let sum = array.reduce((sum, cur) => sum + cur);
+    let result = sum/array.length;
+    return result;
+  }
+
 }
+
+
+
+
+
+
+
+
+
